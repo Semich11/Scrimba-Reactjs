780edd7ebc808ef07c2d0e5e1d067e9f7a379d5e
semich11 <chima11christopher@gmail.com>
2025-06-11 12:16:55 +0100

    Finish Travel App.

--- src/appComponent/appPage.jsx
+++ src/appComponent/appPage.jsx
@@ -4,18 +4,7 @@ import data from "./components/EntryData.js";
 
 export const App = () => {
   const entryData = data.map((entry) => {
-    return (
-      <Entry
-        // name={entry.title}
-        // desc={entry.text}
-        // date={entry.dates}
-        // href={entry.googleMapsLink}
-        // img={entry.img}
-        // country={entry.country}
-        key={entry.id}
-        entry={entry}
-      />
-    );
+    return <Entry key={entry.id} entry={entry} />;
   });
 
   return (
