3aadf80641b43b9d1517f9f518904b3c008627ac
semich11 <chima11christopher@gmail.com>
2025-06-17 10:52:03 +0100

    Started Get recipe placeholder challenge.

--- src/chefClaude/main.jsx
+++ src/chefClaude/main.jsx
@@ -2,7 +2,21 @@ import React from "react"
 
 export default function Main() {
 
-    const [ingredients, setIngredients] = React.useState([])
+    const [ingredients, setIngredients] = React.useState(
+        ["all the main spices", "pasta", "ground beef", "tomato paste"]
+    )
+    /**
+     * Challenge:
+     * 1. Create a boolean state that, for now, will represent whether
+     *    we've gotten a recipe back from the "chef". Default to `false`.
+     *    Can call it `recipeShown`.
+     * 2. Grab the markup in recipeCode.md and paste it below. This will
+     *    be a placeholder for the content that will come back from the 
+     *    chef once we set up that feature.
+     * 3. When the user clicks the "Get a recipe" button, flip the
+     *    `recipeShown` state to true.
+     * 4. Only display the recipe code content if `recipeShown` is true.
+     */
 
     const ingredientsListItems = ingredients.map(ingredient => (
         <li key={ingredient}>{ingredient}</li>
@@ -12,12 +26,6 @@ export default function Main() {
         const newIngredient = formData.get("ingredient")
         setIngredients(prevIngredients => [...prevIngredients, newIngredient])
     }
-    
-    /**
-     * Challenge:
-     * Using conditional rendering, only render the new <section> IF
-     * there are ingredients added to the list of ingredients.
-     */
 
     return (
         <main>
@@ -30,21 +38,18 @@ export default function Main() {
                 />
                 <button>Add ingredient</button>
             </form>
-            {ingredients.length > 0 && 
-                <section>
-                    <h2>Ingredients on hand:</h2>
-                    <ul className="ingredients-list" aria-live="polite">{ingredientsListItems}</ul>
-                    {ingredients.length > 3 &&
-                        <div className="get-recipe-container">
-                            <div>
-                                <h3>Ready for a recipe?</h3>
-                                <p>Generate a recipe from your list of ingredients.</p>
-                            </div>
-                            <button>Get a recipe</button>
-                        </div>
-                    }
-                </section>
-            }
+            {ingredients.length > 0 && <section>
+                <h2>Ingredients on hand:</h2>
+                <ul className="ingredients-list" aria-live="polite">{ingredientsListItems}</ul>
+                {ingredients.length > 3 && <div className="get-recipe-container">
+                    <div>
+                        <h3>Ready for a recipe?</h3>
+                        <p>Generate a recipe from your list of ingredients.</p>
+                    </div>
+                    <button>Get a recipe</button>
+                </div>}
+            </section>}
+            {/** Paste recipeCode.md contents here */}
         </main>
     )
 }
