939b6f9e9219cc928b5f8ef0df5f422a95e791b7
semich11 <chima11christopher@gmail.com>
2025-06-17 10:50:45 +0100

    Done with two conditional rendering challenges.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import { createRoot } from "react-dom/client"
-import { App } from "./joke/app"
 // import { Header } from "./custom_component_quiz/header"
 // import { MainComponent } from "./custom_component_quiz/mainComponent"
 // import { Footer } from "./custom_component_quiz/footer"
 // import './index.css'
 // import { App } from "./appComponent/appPage"
+import { App } from "./joke/app"
+import Main from "./chefClaude/main"
 
 const root = createRoot(document.getElementById("root"))
 
@@ -13,7 +14,8 @@ const root = createRoot(document.getElementById("root"))
 function CustomPage(){
   return(
     <>
-      <App />
+      <Main />
+      
     </>
   )
 }
