61b157f761fd8e85427d3b8329aff5e1b5659e06
semich11 <chima11christopher@gmail.com>
2025-06-18 16:10:10 +0100

    Done with sound pads challenge, part 4.1.

--- src/soundPads/padButton.jsx
+++ src/soundPads/padButton.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import { useState } from 'react'
 
 const PadButton = ({color, on}) => {
+
+  const [lightState, setLightState] = useState(on);
+
+  const lightToggle = () => {
+    console.log(lightState)
+    setLightState(prevLightState => !prevLightState);
+    console.log(lightState)
+  }
+
+  /**
+ * Challenge: Create state controlling whether
+ * this pad is "on" or "off". Use the incoming
+ * `props.on` to determine the initial state.
+ * 
+ * Create an event listener so when the pad is clicked,
+ * it toggles from "on" to "off".
+ * 
+ * Goal: clicking each pad should toggle it on and off.
+ */
+
   return (
-    <button style={{backgroundColor: color}} className={on ? "on" : ""} >
+    <button onClick={lightToggle} style={{backgroundColor: color}} className={lightState ? "on" : ""} >
     </button>
   )
 }
