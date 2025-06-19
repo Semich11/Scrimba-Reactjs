d42b01f640645855c3409f5ec00420fc0bc8801a
semich11 <chima11christopher@gmail.com>
2025-06-19 15:41:05 +0100

    Edited Chef Claude challenge: refactor to separate components

--- src/chefClaude/claudeRecipe/ingredientsList.jsx
+++ src/chefClaude/claudeRecipe/ingredientsList.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import Ingredients from "./ingredients";
 
-export default function Recipe (props) {
+export default function IngredientsList (props) {
+      const ingredientsListItems = props.ingredients.map(ingredient => (
+        <li key={ingredient}>{ingredient}</li>
+    ))
   return (
     <section>
       <h2>Ingredients on hand:</h2>
       <ul className="ingredients-list" aria-live="polite">
-        {props.ingredientsListItems}
+        {ingredientsListItems}
       </ul>
       {props.ingredients.length > 3 && <Ingredients toggleRecipeShown={props.toggleRecipeShown} />}
     </section>
