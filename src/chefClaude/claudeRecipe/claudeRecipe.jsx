a75348e8c4d479752a1cb2408cbce2c35f2c74d3
semich11 <chima11christopher@gmail.com>
2025-06-20 17:44:55 +0100

    Completed Scrimba React State Section.'

--- src/chefClaude/claudeRecipe/claudeRecipe.jsx
+++ src/chefClaude/claudeRecipe/claudeRecipe.jsx
@@ -5,7 +5,8 @@ export default function ClaudeRecipe({ AIRecipe }) {
   const markdown = AIRecipe;
 
   return (
-    <section className="suggested-recipe-container">
+    <section className="suggested-recipe-container" aria-live="polite">
+      <h2>Chef Claude Recommends:</h2>
       <Markdown>{markdown}</Markdown>
     </section>
   );
