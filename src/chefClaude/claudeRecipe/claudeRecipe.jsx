be8876623deb2c19df47ac67a1144179b8abfdce
semich11 <chima11christopher@gmail.com>
2025-06-20 17:25:07 +0100

    Done with format recipe response challenge.

--- src/chefClaude/claudeRecipe/claudeRecipe.jsx
+++ src/chefClaude/claudeRecipe/claudeRecipe.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import Markdown from 'react-markdown'
 
 export default function ClaudeRecipe({AIRecipe}) {
+  const markdown = AIRecipe
+  /**
+ * Challenge: See if you can figure out how to use the
+ * react-markdown package to render `props.recipe` as React
+ * elements rather than the plain markdown text.
+ * 
+ * Use the link in the slide to navigate directly to the
+ * package's instructions on how to use it.
+ */
   return (
-    <div>
-        {AIRecipe}
-    </div>
+    <Markdown>
+        {markdown}
+    </Markdown>
   )
 }
 
